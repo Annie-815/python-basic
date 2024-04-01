2be1e3b7dc258f8d5fe69b5b77ac5a3f1b1c1c3f
Annie-815 <76701699+Annie-815@users.noreply.github.com>
2024-04-01 08:55:03 +0300

    conditionals

---
+++ temp.py
@@ -0,0 +1,9 @@
+temperature = int(input("temperature: "))
+if temperature > 30:
+    print("its a hot day")
+    print("ensure you drink alot of water")
+elif temperature < 10:
+    print("its a cold day")
+    print("wear warm clothing")
+else:
+    print("its neither hot nor cold")
