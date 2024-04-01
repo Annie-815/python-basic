ef07955ec65fae97f569fb11f4166eb7e5c19535
Annie-815 <76701699+Annie-815@users.noreply.github.com>
2024-04-01 08:50:39 +0300

    arithmetic operations

---
+++ main2.py
@@ -0,0 +1,15 @@
+import math
+#arithmetic operations in python
+x = 8
+y = 5
+sum1 = x + y
+product = x * y
+difference = x - y
+div = x / y
+print(sum1)
+print(product)
+print(difference)
+print(div)
+print(round(div))
+print(math.ceil(div))
+print(math.floor(div))
